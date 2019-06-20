e3c5bc1a8f9e16b6c768932e619e8c6d90ead547
Rafael Barros <rafaelbarros.df@gmail.com>
2019-06-20 14:26:12 -0300

    :construction: 2.7 - Buscando um livro com o uso da anotacao PathVariable

--- src/main/java/com/algaworks/socialbooks/api/resource/LivroResource.java
+++ src/main/java/com/algaworks/socialbooks/api/resource/LivroResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +28,10 @@ public class LivroResource {
 	@PostMapping
 	public void salvar(@RequestBody Livro livro) {
 		livroRepository.save(livro);
+	}
+	
+	@GetMapping("/{id}")
+	public Livro buscar(@PathVariable("id") Long id) {
+		return livroRepository.findOne(id);
 	}
 }
