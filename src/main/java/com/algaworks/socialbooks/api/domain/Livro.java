abb7bb2eda1c14c85bcc1a0448d68a028022e909
Rafael Barros <rafaelbarros.df@gmail.com>
2019-06-20 10:42:46 -0300

    :construction: 2.4 - Utilizando a anotacao JsonInclude

--- src/main/java/com/algaworks/socialbooks/api/domain/Livro.java
+++ src/main/java/com/algaworks/socialbooks/api/domain/Livro.java
@@ -3,23 +3,32 @@ package com.algaworks.socialbooks.api.domain;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Data;
 
 @Data
 public class Livro {
 
+	@JsonInclude(Include.NON_NULL)
 	private Long id;
 	
 	private String nome;
 	
+	@JsonInclude(Include.NON_NULL)
 	private LocalDate publicacao;
 	
+	@JsonInclude(Include.NON_NULL)
 	private String editora;
 	
+	@JsonInclude(Include.NON_NULL)
 	private String resumo;
 	
+	@JsonInclude(Include.NON_NULL)
 	private List<Comentario> comentarios;
 	
+	@JsonInclude(Include.NON_NULL)
 	private String autor;
 	
 	public Livro(String nome) {
