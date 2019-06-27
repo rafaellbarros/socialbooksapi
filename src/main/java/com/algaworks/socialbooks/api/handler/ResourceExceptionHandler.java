cfe0df999e6888b9c066045ae36885a2da1ba400
Rafael Barros <rafaelbarros.df@gmail.com>
2019-06-26 22:55:29 -0300

    :construction: 2.13 - Manipulando erros com ExceptionHandler e ControllerAdvice v2

--- src/main/java/com/algaworks/socialbooks/api/handler/ResourceExceptionHandler.java
+++ src/main/java/com/algaworks/socialbooks/api/handler/ResourceExceptionHandler.java
@@ -7,22 +7,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.algaworks.socialbooks.api.domain.DetalhesErro;
 import com.algaworks.socialbooks.api.service.exception.LivroNaoEncontradoException;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(LivroNaoEncontradoException.class)
-	public ResponseEntity<DetalhesErro> handleLivroNaoEncontradoException
-			(LivroNaoEncontradoException e, HttpServletRequest request) {
-		
-		DetalhesErro erro = new DetalhesErro();
-		erro.setStatus(404l);
-		erro.setTitulo("O livro não pôde ser encontrado");
-		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
-		erro.setTimestamp(System.currentTimeMillis());
-		
+	public ResponseEntity<Erro> handleLivroNaoEncontradoException
+			(LivroNaoEncontradoException ex, HttpServletRequest request) {
+	
+		String mensagemUsuario = ex.getMessage();
+		String mensagemDesenvolvedor = ex.toString();
+
+		Erro erro = new Erro(mensagemUsuario, mensagemDesenvolvedor, 404l);
+						
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@Getter
+	@AllArgsConstructor
+	public static class Erro {
+		
+		private String mensagemUsuario;
+		private String mensagemDsenvolvedor;
+		private Long status;
 	}
 }
