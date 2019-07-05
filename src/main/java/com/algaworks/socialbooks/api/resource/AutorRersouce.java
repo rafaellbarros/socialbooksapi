18fe3a6e827dc39baf39c06acb6f53ba33937f8e
Rafael Barros <rafaelbarros.df@gmail.com>
2019-07-04 22:53:24 -0300

    :construction: 2.20 - Negociacao de conteudos com Media Type

--- src/main/java/com/algaworks/socialbooks/api/resource/AutorRersouce.java
+++ src/main/java/com/algaworks/socialbooks/api/resource/AutorRersouce.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ public class AutorRersouce {
 	@Autowired
 	private AutorService autorService;
 	
-	@GetMapping
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<List<Autor>> listar() {
 		List<Autor> autores = autorService.listarAutores();
 		return ResponseEntity.status(HttpStatus.OK).body(autores);
