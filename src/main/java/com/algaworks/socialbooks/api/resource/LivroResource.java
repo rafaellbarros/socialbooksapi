eb34ce35571b5f4031919910b3cceb3501d5efbd
Rafael Barros <rafaelbarros.df@gmail.com>
2019-07-11 22:52:31 -0300

    :construction: 4.6 Cross Origin Resource Sharing CORS

--- src/main/java/com/algaworks/socialbooks/api/resource/LivroResource.java
+++ src/main/java/com/algaworks/socialbooks/api/resource/LivroResource.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ public class LivroResource {
 	@Autowired
 	private LivroService livroService;
 
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<Livro>> listar() {
 		return ResponseEntity.status(HttpStatus.OK).body(livroService.listar());
